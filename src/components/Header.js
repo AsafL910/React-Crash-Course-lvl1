e1ca121c042a0b26e7a6c39fc7ba65c0e3e93444
AsafL910 <asaflumer@gmail.com>
2022-01-18 10:50:04 +0200

    create first component and add CSS file

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>Task Tracker</h1>
+        </header>
+    )
+}
+
+//CSS
+const headerStyle = {
+    color: 'black',
+    backgroundColor: 'white'
+}
+export default Header
